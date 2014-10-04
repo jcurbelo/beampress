797df85239024076a83b9fc804cee57a43e109ee
jcurbelo <curbelorobin@gmail.com>
2014-10-03 20:44:07 -0700

    Adding 'data-againframe' functionalities.

--- js/beampress.js
+++ js/beampress.js
@@ -15,13 +15,18 @@
 
     //Process all presentation's elements
     var processFrames = function() {
+        var againFrames = []
     	$('.frame').each(function (i) {
+            var hasAgainFrame = false;
             var hasOnSlide = false;
             //Checks for frames [data-onslide]
             // For some browsers, `attr` is undefined; for others,
             // `attr` is false.  Check for both.
             if ($(this).attr('data-onslide') !== undefined && $(this).attr('data-onslide') !== false)
                 hasOnSlide = true;
+
+            if ($(this).attr('data-againframe') !== undefined && $(this).attr('data-againframe') !== false)
+                hasAgainFrame = true;            
 
             var frame = {'item' : $(this)};
             frame.slideIntervals = hasOnSlide ? getSlideIntervals($(this), i) : [];
@@ -36,13 +41,44 @@
     		});
             //Hiding current frame
     		$(this).css('display', 'none');
+
+            //Adding the 'againframe' to the list of pendings againframes
+            if(hasAgainFrame){
+                var reg = new RegExp(/\s*\[(.*)\]\s*(.*)/);
+                var matches = reg.exec($(this).attr('data-againframe'));
+                var id = matches[1];
+                var intervals = matches[2];               
+                var frame = getFrameFromStrId(id);
+                if (frame !== null){
+                    againFrames.push({'pos' : i + 1, 'frame' : frame, 'intervals': intervals});
+                }    
+            }
     	});
     	//Showing first slide
 		frames[currentFrame].item.css('display', 'block');
+        //Processing pending againFrames
+        proccessAgainFrames(againFrames);
 		//Increasing first slide show
     	next();
     };
 
+
+    var proccessAgainFrames = function (againFrames){
+        againFrames.forEach(function (af) {
+            //Inserting the new frame
+            frames.splice(af.pos, 0, af.frame);
+            //Adding the same items that previous frame have
+            framesItems.splice(af.pos, 0, framesItems[af.pos - 1]);
+            //Updating the new intervals if they exists
+            af.frame.slideIntervals = af.intervals ? getSlideIntervals(af.intervals, af.pos) : [];
+
+
+        });
+    }
+
+
+    //Returns a new copy of the frame that
+    //has 'str' Id
     var getFrameFromStrId = function (str){
         var frame = null;
         frames.forEach(function (f){
@@ -62,13 +98,16 @@
         return frame;
     }
 
-    //Returns the slide intervals given a selector (item)
-    var getSlideIntervals = function (item, frameIndex){
-        var slideIntervals = []        
-        var slides = item.attr('data-onslide').split(',');
-        //Having issues with \d (don't know why)
-        var reg = new RegExp( '([1-9][0-9]*)?(-)?([1-9][0-9]*)?')
-        slides.forEach(function (slide) {
+    //Returns the slide intervals given a selector 
+    //or an array of slides (bag)
+    var getSlideIntervals = function (bag, frameIndex){
+        var slideIntervals = []
+        if (!(typeof bag === 'string'))      
+           bag = bag.attr('data-onslide').split(',');
+        else
+            bag = bag.split(',');
+        var reg = new RegExp(/(\s*[1-9]\d*)?(\s*-\s*)?(\s*[1-9]\d*\s*)?/);
+        bag.forEach(function (slide) {
             var interval = {};
             var matches = reg.exec(slide);
             //Only one slide
@@ -181,7 +220,7 @@
         // console.log(frames);
         // console.log(lastPerFrame);
         // console.log(framesItems);
-        console.log(getFrameFromStrId('f-frame'));
+        // console.log(getFrameFromStrId('f-frame'));
     	//triggering key up
 		$(document).keyup(function (event){
 
