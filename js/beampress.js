4599f474cb3ad615f89064729b6ae35c3ca9e458
jcurbelo <curbelorobin@gmail.com>
2014-10-03 18:50:19 -0700

    Updating last slide for frames that have 'data-onslide' attr.

--- js/beampress.js
+++ js/beampress.js
@@ -16,11 +16,20 @@
     //Process all presentation's elements
     var processFrames = function() {
     	$('.frame').each(function (i) {
-            var frame = getSlideItem($(this), null);
+            var hasOnSlide = false;
+            //Checks for frames [data-onslide]
+            // For some browsers, `attr` is undefined; for others,
+            // `attr` is false.  Check for both.
+            if ($(this).attr('data-onslide') !== undefined && $(this).attr('data-onslide') !== false)
+                hasOnSlide = true;
+
+            var frame = {'item' : $(this)};
+            frame.slideIntervals = hasOnSlide ? getSlideIntervals($(this), i) : [];
     		frames[i] = frame;
             framesItems[i] = [];
     		$(this).find('[data-onslide]').each(function (j) {
-    			var slideItem = getSlideItem($(this), i);
+    			var slideItem = {'item' : $(this)};
+                slideItem.slideIntervals = getSlideIntervals($(this), hasOnSlide ? null : i);
     			framesItems[i].push(slideItem);
     			//Hiding item
     			$(this).css('opacity', 0);
@@ -37,15 +46,7 @@
     //Returns the slide intervals given a selector (item)
     var getSlideIntervals = function (item, frameIndex){
         var slideIntervals = []        
-        //Checks for frames [data-onslide]
-        var slides = item.attr('data-onslide');
-        // For some browsers, `attr` is undefined; for others,
-        // `attr` is false.  Check for both.
-        if (slides !== undefined && slides !== false){
-            slides = slides.split(',');
-        } else {
-            slides = [];
-        } 
+        var slides = item.attr('data-onslide').split(',');
         //Having issues with \d (don't know why)
         var reg = new RegExp( '([1-9][0-9]*)?(-)?([1-9][0-9]*)?')
         slides.forEach(function (slide) {
@@ -60,26 +61,17 @@
                 interval.lower = matches[1] || 0;
                 interval.upper = matches[3] || -1;
             }
-            if (frameIndex !== null){
-                 lastPerFrame[frameIndex] = 0;
-                 lastPerFrame[frameIndex] = Math.max(lastPerFrame[frameIndex], interval.upper);
-                 lastPerFrame[frameIndex] = Math.max(lastPerFrame[frameIndex], interval.lower);
+            //Updating last slide per frame
+            if(frameIndex !== null){
+                lastPerFrame[frameIndex] = 0;
+                lastPerFrame[frameIndex] = Math.max(lastPerFrame[frameIndex], interval.upper);
+                lastPerFrame[frameIndex] = Math.max(lastPerFrame[frameIndex], interval.lower);
             }
             slideIntervals.push(interval);
         });
 
         return slideIntervals;               
     }
-
-    //Returns a 'slideItem' object containing
-    //the html selector and slide intervals given
-    //[data-onslide] attr
-    var getSlideItem = function (item, frameIndex){
-    	var slideItem = {};
-    	slideItem.item = item;
-    	slideItem.slideIntervals = getSlideIntervals(item, frameIndex);
-		return slideItem;
-    };
 
     //Show all slide items that are 'present' on
     //next slide 
@@ -107,12 +99,26 @@
     //and hide all that aren't
     var updateSlideItems = function () {
     	//Updating all items status
+        //TODO: Think about doing events
     	framesItems[currentFrame].forEach(function (slideItem){
     		var flag = false;
     		slideItem.slideIntervals.forEach(function (interval){
     			if (interval.lower <= currentSlide && (interval.upper == -1 || interval.upper >= currentSlide)){
-    				flag = true;
-    				return false;
+
+                    //Frames also have intervals
+                    var frameIntervals = frames[currentFrame].slideIntervals;
+                    if(frameIntervals.length > 0) 
+                        frameIntervals.forEach(function (fi) {
+                            if (fi.lower <= currentSlide && (fi.upper == -1 || fi.upper >= currentSlide)){
+                                flag = true;
+                                return false;
+                            }
+                        });
+                    else
+                        flag = true;
+
+    				if(flag)
+    				    return false;
     			}
     		});
 
@@ -153,7 +159,9 @@
 
     $.fn.beampress = function (options){
     	processFrames();
-
+        // console.log(frames);
+        // console.log(lastPerFrame);
+        // console.log(framesItems);
     	//triggering key up
 		$(document).keyup(function (event){
 
